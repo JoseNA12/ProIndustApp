22d9f120efdb4526337f9ff43497f40a71f87473
José Navarro <josenava@estudiantec.cr>
2018-05-14 10:12:00 -0600

    initial commit

---
+++ app/src/main/java/reque/proyecto2/jose_/proindust_app/IniciarSesionActivity.java
@@ -0,0 +1,13 @@
+package reque.proyecto2.jose_.proindust_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
