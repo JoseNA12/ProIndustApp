c880cbb443c5e83b705bc803beb977c2b08bb3d9
José Navarro <josenava@estudiantec.cr>
2018-05-14 14:43:34 -0600

    ...

--- app/src/main/java/reque/proyecto2/jose_/proindust_app/IniciarSesionActivity.java
+++ app/src/main/java/reque/proyecto2/jose_/proindust_app/IniciarSesionActivity.java
@@ -1,13 +1,29 @@
 package reque.proyecto2.jose_.proindust_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+    Button boton_Ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        boton_Ingresar = (Button)findViewById(R.id.bt_ingresar_ID);
+
+        boton_Ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent_menuPrincipal = new Intent(IniciarSesionActivity.this, MenuPrincipal.class);
+                startActivity(intent_menuPrincipal);
+            }
+        });
     }
 }
