eda14f0c11ea5d2b26a01879352e85727a921461
José Navarro <josenava@estudiantec.cr>
2018-05-14 21:55:49 -0600

    ...

--- app/src/main/java/reque/proyecto2/jose_/proindust_app/MenuPrincipal.java
+++ app/src/main/java/reque/proyecto2/jose_/proindust_app/MenuPrincipal.java
@@ -17,12 +17,6 @@ import android.view.MenuItem;
 public class MenuPrincipal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    // YO AGREGUÉ ESTO ----------------------------------------------------------------------------
-    private  FloatingActionButton fabInfo,fabAbout;
-    private  LinearLayoutCompat layoutAbout;
-    private  boolean fabExpanded;
-    // ----------------------------------------------------------------------------
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,17 +82,11 @@ public class MenuPrincipal extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_info_contacto) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_cerrar_sesion)
+        {
 
         }
 
